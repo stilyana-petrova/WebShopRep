d458f99323dc72735103b99dda40161be9e32d38
stilyana-petrova <stilyanapetrova1@gmail.com>
2023-02-27 13:37:12 +0200

    part 5

---
+++ WebShopDemo/WebShopDemo/Models/Order/OrderIndexVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShopDemo.Models.Order
+{
+    public class OrderIndexVM
+    {
+        public int Id { get; set; }
+        public string OrderDate { get; set; }
+        public string UserId { get; set; }
+        public string User { get; set; }
+        public int ProductId { get; set; }
+        public string Product { get; set; }
+        public string Picture { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public decimal TotalPrice { get; set; }
+
+    }
+}
